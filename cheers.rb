460ad2573c9250c7e021f71c62f29c287db656b5
Cody Spriggs <codyspriggs@me.com>
2013-04-16 14:01:25 -0500

    Added Regex for Vowels

--- cheers.rb
+++ cheers.rb
@@ -1,6 +1,30 @@
 puts "WHAT.....is your name?"
 input = gets.chomp
-name = input.each_char { |letter|
-	puts "Gimme a #{letter}!"
-}
-puts "What does that spell?! #{input}!"
+puts input.upcase!
+
+input.each_char do |letter|
+	vowel = /[AEIOU]/
+	if letter.match(vowel)
+		puts "Gimme an #{letter}!"
+	else
+		puts "Gimme a #{letter}!"
+	end
+end
+puts "What does that spell?! #{input.upcase}!"
+
+#name = input.each_char do |letter|
+#	article = "a"
+#	if letter == ("a" || "e" || "i" || "o" || "u") 
+#		article = "an"
+#		puts "Gimme #{article} #{letter.upcase}!"
+#	else
+#		puts "Gimme #{article} #{letter.upcase}!"
+#	end
+#end
+
+# try using downcase.input.each_char do |letter|
+
+# .include? for vowels/consonants
+
+#Cheer = "Give me a "
+#name.upcase.each char { |char| puts cheer + char <<"!"}
